e3c0be0fed740be6c52f594fc52101a43f3fe0c0
sabinlungudotcpp <sabinlungu293@gmail.com>
2020-10-21 21:06:29 +0100

    Adding validation to the Message class.

--- NapierBankMessageFilteringSystem/BusinessLayer/Message.cs
+++ NapierBankMessageFilteringSystem/BusinessLayer/Message.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,5 +13,46 @@ namespace NapierBankMessageFilteringSystem.BusinessLayer
     {
         private string messageID;
         private string messageBody;
+
+        public string MessageID
+        {
+            set
+            {
+               if(isIdValid())
+                {
+                    this.messageID = value.ToUpper(); // Set the message ID to uppercase if it's valid
+                }
+
+                else
+                {
+                    throw new ArgumentException("Message ID is invalid. It must start with S, E or T");
+                }
+            }
+
+            get
+            {
+                return this.messageID;
+            }
+        }
+
+        public string MessageBody
+        {
+            set
+            {
+
+            }
+
+            get
+            {
+                return this.messageBody;
+            }
+        }
+
+        public bool isIdValid() // Determines if the Message ID is valid or not
+        {
+            return messageID.Length == 10 && int.TryParse(messageID.Remove(0, 1), out _);
+        }
+
+
     }
 }
