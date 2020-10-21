f02335519e285952bc4aed6e638b32ea8653556e
Sabin Constantin Lungu (2B || !2B) <sabinlungu293@gmail.com>
2020-10-21 18:57:21 +0100

    Add files via upload

---
+++ NapierBankMessageFilteringSystem/BusinessLayer/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NapierBankMessageFilteringSystem.BusinessLayer
+{
+    // Author: Sabin Constantin Lungu
+    // Date of creation: 21/10/2020
+    public class Message // Message Class.
+    {
+
+    }
+}
