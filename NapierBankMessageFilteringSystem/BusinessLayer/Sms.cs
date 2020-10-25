8d5d4b4290c28cca07ec1bd5913015e52e975753
sabinlungudotcpp <sabinlungu293@gmail.com>
2020-10-25 19:03:55 +0000

    Adding Business layer code for the Country Code & validation

--- NapierBankMessageFilteringSystem/BusinessLayer/Sms.cs
+++ NapierBankMessageFilteringSystem/BusinessLayer/Sms.cs
@@ -7,8 +7,50 @@ using System.Threading.Tasks;
 
 namespace NapierBankMessageFilteringSystem.BusinessLayer
 {
-    public class Sms : Message
+    public class Sms : Message // SMS class inherits feeatures from the Message base class.
     {
+        private string regexMatcher = "^([0|/+[0-9]{1,5})?([7-9][0-9]{9})$";
+        private string sender;
+        private string countryCode; // The SMS country code
+        private string smsText; // The SMS text
 
+        public string Sender
+        {
+            set
+            {
+                if(isSenderValid())
+                {
+                    this.sender = value;
+                }
+
+                else if(!isSenderValid())
+                {
+                    throw new ArgumentException("Sender is invalid. Please re-enter");
+                }
+            }
+
+            get
+            {
+                return this.sender;
+            }
+        }
+
+        public string CountryCode
+        {
+            set
+            {
+
+            }
+
+            get
+            {
+                return this.countryCode;
+            }
+        }
+
+        private bool isSenderValid()
+        {
+            return Regex.IsMatch(this.sender, regexMatcher); // Returns true or false if the sender matches the regular expressions
+        }
     }
 }
