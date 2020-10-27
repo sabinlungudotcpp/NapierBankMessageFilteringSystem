621c6044b632374b9c45ecb540317bc702692b79
sabinlungudotcpp <sabinlungu293@gmail.com>
2020-10-27 12:14:00 +0000

    Added code to read in the text words file

--- NapierBankMessageFilteringSystem/DataLayer/Abbreviations.cs
+++ NapierBankMessageFilteringSystem/DataLayer/Abbreviations.cs
@@ -1,28 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace NapierBankMessageFilteringSystem.DataLayer
 {
     public class Abbreviations // Abbreviations class that replaces an abbreviation with its actual definition
     {
-        public List<string> messagesList;
-        public List<string> abbreviationsList;
-        private bool foundAbbreviation;
-        private string filePath = "";
+        public List<string> listOfDefinitions = new List<string>();
+        public List<string> abbreviationsList = new List<string>();
+
+        private bool foundAbbreviation = false;
+        private bool isFileRead = false;
+        private string textWordsFile = "C:/Users/const/Desktop/NapierBankMessageFilteringSystem/textwords.csv";
+
+        public void readFile() // Read the file that contains the abbreviations
+        {
+            StreamReader abbreviationsFile = new StreamReader(textWordsFile); // Create a stream reader to read in the file
+            string fileLine;
+
+            while((fileLine = abbreviationsFile.ReadLine()) != null)
+            {
+                string[] textValues = fileLine.Split(',');
+
+                abbreviationsList.Add(textValues[0]);
+                listOfDefinitions.Add(textValues[1]);
+
+                isFileRead = true; // File is read successfully
+            }
+
+            foreach(string def in listOfDefinitions)
+            {
+                MessageBox.Show(def.ToString());
+            }
+        }
+
+        public string replaceMessage(string sentence)
+        {
+            return "";
+        }
+
+
     }
-
-    public void readFile() // Read the file that contains the abbreviations
-    {
-
-    }
-
-    public string replaceMessage(string sentence)
-    {
-        return "";
-    }
-
-
 }
--- NapierBankMessageFilteringSystem/MainWindow.xaml.cs
+++ NapierBankMessageFilteringSystem/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NapierBankMessageFilteringSystem.DataLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,7 +29,11 @@ namespace NapierBankMessageFilteringSystem
 
         private void readFileBtn_Click(object sender, RoutedEventArgs e)
         {
+            Abbreviations abb = new Abbreviations();
 
+            abb.readFile();
         }
+
+        
     }
 }
