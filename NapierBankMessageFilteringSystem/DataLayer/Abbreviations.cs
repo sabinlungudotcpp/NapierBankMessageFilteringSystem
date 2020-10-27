800fe7edf4db942a96da4a6f927fdb5552e12e69
sabinlungudotcpp <sabinlungu293@gmail.com>
2020-10-27 11:43:52 +0000

    Adding code to read the abbreviations file and replace them

---
+++ NapierBankMessageFilteringSystem/DataLayer/Abbreviations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NapierBankMessageFilteringSystem.DataLayer
+{
+    public class Abbreviations // Abbreviations class that replaces an abbreviation with its actual definition
+    {
+        public List<string> messagesList;
+        public List<string> abbreviationsList;
+        private bool foundAbbreviation;
+        private string filePath = "";
+    }
+
+    public void readFile() // Read the file that contains the abbreviations
+    {
+
+    }
+
+    public string replaceMessage(string sentence)
+    {
+        return "";
+    }
+
+
+}
